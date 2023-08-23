ae55ae43bfaae078768a60db386206f6b9ad480c
Sota Giorgadze <giorgadze@inbox.lv>
2023-08-23 11:27:33 +0200

    5:04:01

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+import Image from "next/image";
 import { redirect } from "next/navigation";
 
 import { currentUser } from "@clerk/nextjs";
@@ -19,7 +21,36 @@ async function Page() {
     <section>
       <h1 className="head-text mb-10">Activity</h1>
 
-      <section></section>
+      <section className="mt-10 flex flex-col gap-5">
+        {activity.length > 0 ? (
+          <>
+            {activity.map((item) => (
+              <Link
+                key={item._id}
+                href={`/thread/${item.parentId}`}
+              >
+                <article className="activity-card">
+                  <Image
+                    width={20}
+                    height={20}
+                    alt="profile picture"
+                    src={item.author.image}
+                    className="rounded-full object-cover"
+                  />
+                  <p className="!text-small-regular text-light-1">
+                    <span className="mr-1 text-primary-500">
+                      {item.author.name}
+                    </span>{" "}
+                    replied to your thread
+                  </p>
+                </article>
+              </Link>
+            ))}
+          </>
+        ) : (
+          <p className="!text-base-regular text-light-3">No activity yet</p>
+        )}
+      </section>
     </section>
   );
 }
